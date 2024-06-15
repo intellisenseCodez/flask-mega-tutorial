ce00ba028a5c9a4b45162ef76c4320d7fdaf906c
Oyekanmi lekan <oyekanmiakande@gmail.com>
2024-06-15 02:25:52 +0100

    configure flask login and registration

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +11,21 @@ class LoginForm(FlaskForm):
     password = PasswordField(label='password', validators=[DataRequired()])
     remember_me = BooleanField(label='Remember Me')
     submit = SubmitField(label='Sign In')
+    
+    
+class RegistrationForm(FlaskForm):
+    username = StringField(label='Username', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    password2 = PasswordField(label='Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(User.username == username.data))
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User).where(User.email == email.data))
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
